7c2f44efb5d612becb89d373120d28ccf9783db5
fenikhey88 <limonprog@gmail.com>
2025-04-15 19:12:22 +0300

    First

---
+++ TI_1lab/TI_1lab/TI_1labApp.swift
@@ -0,0 +1,17 @@
+//
+//  TI_1labApp.swift
+//  TI_1lab
+//
+//  Created by Fenik Hey on 05.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TI_1labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TI_1lab/TI_1labTests/TI_1labTests.swift
@@ -0,0 +1,17 @@
+//
+//  TI_1labTests.swift
+//  TI_1labTests
+//
+//  Created by Fenik Hey on 05.04.2025.
+//
+
+import Testing
+@testable import TI_1lab
+
+struct TI_1labTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
